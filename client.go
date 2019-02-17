39f262f7b62dd9ea96942efef89e683fe7a02d38
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2019-02-17 18:01:19 +0100

    feat: get server address from args

--- client.go
+++ client.go
@@ -1,8 +1,22 @@
 package main
 
-import "battleship/battleship"
+import (
+	"battleship/battleship"
+	"log"
+	"os"
+	"regexp"
+)
 
 func main() {
-	client := battleship.Connect("127.0.0.1")
-	client.Play()
+	addr := "127.0.0.1"
+	if len(os.Args) > 1 {
+		addr = os.Args[1]
+		r := regexp.MustCompile("(?:[0-9]{0,3}\\.){3}[0-9]{1,3}$")
+
+		if !r.MatchString(addr) {
+			log.Fatal(addr, " is not a valid TCP4 Address!")
+		}
+	}
+
+	battleship.Connect(addr).Play()
 }
