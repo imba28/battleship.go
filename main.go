1d66dacfc1eedf440558a1a5481879e071a5ac72
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2019-02-19 10:34:08 +0100

    refactor: merge client and server entry point

--- main.go
+++ main.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"battleship/battleship"
+	"flag"
+	"log"
 	"math/rand"
+	"regexp"
 	"time"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	server := battleship.Server{}
-	server.Run()
+	isClient := flag.Bool("client", true, "start as client")
+	addr := flag.String("server", "127.0.0.1", "ip address of the server")
+	flag.Parse()
+
+	if *isClient {
+		r := regexp.MustCompile("(?:[0-9]{0,3}\\.){3}[0-9]{1,3}$")
+
+		if !r.MatchString(*addr) {
+			log.Fatal(addr, " is not a valid TCP4 Address!")
+		}
+
+		c := battleship.Connect(*addr)
+		c.Play()
+	} else {
+		server := battleship.Server{}
+		server.Run()
+	}
+
 }
